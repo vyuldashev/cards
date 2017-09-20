201bce291f7b4eb4c5352e4d19f815d2d5de3204
vyuldashev <misterio92@gmail.com>
2017-09-20 12:02:09 +0300

    wip

--- src/Exceptions/InvalidPanException.php
+++ src/Exceptions/InvalidPanException.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Vyuldashev\Cards\Exceptions;
@@ -7,5 +8,4 @@ use Exception;
 
 class InvalidPanException extends Exception
 {
-
 }
--- src/Luhn.php
+++ src/Luhn.php
@@ -11,10 +11,10 @@ class Luhn
         $length = mb_strlen($number);
         $sum = 0;
         for ($i = $length - 1; $i >= 0; $i -= 2) {
-            $sum += $number{$i};
+            $sum += $number[$i];
         }
         for ($i = $length - 2; $i >= 0; $i -= 2) {
-            $sum += array_sum(str_split((string)($number{$i} * 2)));
+            $sum += array_sum(str_split((string)($number[$i] * 2)));
         }
 
         return $sum % 10 === 0;
